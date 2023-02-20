74dbac92d64c8deb7b021217cb11b85d2c2bef9e
elitaldev <eligoli1007@yahoo.co.uk>
2023-02-20 15:14:42 +0000

    style popup message and start js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let btnRef = document.querySelectorAll(".btn-option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+
+//pattern array//
+let winningPattern = 
